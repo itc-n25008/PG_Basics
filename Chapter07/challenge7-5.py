2b95466a70906f260706a72ef781259212f77710
n25008 <n25008@std.it-college.ac.jp>
2025-06-12 01:03:26 +0000

    README.mdに第8章を追記

--- Chapter07/challenge7-5.py
+++ Chapter07/challenge7-5.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-numbers = [11, 32, 33, 15, 1]
-
-while True:
-    answer = input("Guess a number or type q to quit.")
-    if answer == "q":
-        break
-    try:
-        answer = int(answer)
-    except ValueError:
-        print("please type a number or q to quit.")
-    if answer in numbers:
-        print("You guessed correctly!")
-    else:
-        print("You guessed incorrectly!")
-=======
 list1 = [8, 19, 148, 4]
 list2 = [9, 1, 33, 83]
 list3 = []
@@ -24,4 +8,3 @@ for i in list1:
         list3.append(i * j)
 
 print(list3)
->>>>>>> CH07
